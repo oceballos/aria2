a68bc47ea79e25b35d1fc45a585ce1ca1edb4bff
oaceballos <oaceballos@gmail.com>
2013-11-28 18:29:10 -0300

    respaldo antes de quitar alpha

--- src/CDNVideoURLparser.sh
+++ src/CDNVideoURLparser.sh
@@ -5,7 +5,7 @@ uri=$1
 #regex2=[[ "$newfile" =~ ([[\ {\(-]*(www\.)?(torrentday\.com|torrenting\.com|spastikustv|speed\.cd|moviesp2p\.com|publichd\.org|publichd|scenetime\.com|kingdom-release)[]\ }\)-]*) ]]
 #regex3='(http\:\/\/)?(www)?\.?(youtube)\.?(com\/)(watch\?)(v\=)[a-zA-Z0-9\_]{11}'
 regex4='((http\:\/\/)|(https\:\/\/))?((www\.)|(w2\.)|(w3\.))?((youtube)\.(com\/)|(youtu)\.(be\/))(watch\?)(v\=)[a-zA-Z0-9\_\-]{11}'
-
+#regex5='((http\:\/\/)|(https\:\/\/))?((www\.)|(w2\.)|(w3\.))?((youtube)\.(com\/)|(youtu)\.(be\/))(watch\?)(v\=)[a-zA-Z0-9\_\-]{11}(&(noredirect=1|list=[A-Z0-9]){18})?'
 
 if [[ "$uri" =~ $regex4 ]] ;
 then
